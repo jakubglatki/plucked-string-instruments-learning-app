27fcfa3cfadda01f5b50a78acb229f9730d3687b
jakubglatki <jakub.glatki@gmail.com>
2020-10-25 23:51:52 +0100

    opening simple view in the browser

--- src/main/java/app/PluckedStringInstrumentsApplication.java
+++ src/main/java/app/PluckedStringInstrumentsApplication.java
@@ -2,20 +2,14 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@RestController
-public class PluckedStringInstrumentsApplication {
+public class PluckedStringInstrumentsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PluckedStringInstrumentsApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+
 }
---
+++ src/main/java/app/ui/MainView.java
@@ -0,0 +1,15 @@
+package app.ui;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public  MainView() {
+        Button click_me = new Button("xd me");
+        add(click_me);
+    }
+}
